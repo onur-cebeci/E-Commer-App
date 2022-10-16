22c532d768e2a27a0e1410e66ea70374641461a5
onur-cebeci <112099545+onur-cebeci@users.noreply.github.com>
2022-10-16 15:39:54 +0300

    I had added some tools(f,rebase CLI , text fonts,constant Colors and textTheme)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:e_commer/constant.dart';
+import 'package:e_commer/firebase_options.dart';
 import 'package:e_commer/screens/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,9 +18,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'E Commer App',
+      debugShowCheckedModeBanner: false,
+      title: 'E Commerce App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: kPrimarySwatch,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+              color: kSecondColor,
+              fontSize: 35,
+              fontWeight: FontWeight.w700,
+              fontFamily: "Serif"),
+          headline2: TextStyle(
+              color: kSecondColor,
+              fontSize: 28,
+              fontWeight: FontWeight.w600,
+              fontFamily: "Serif"),
+          headline3: TextStyle(
+              color: kSecondColor,
+              fontSize: 23,
+              fontWeight: FontWeight.w400,
+              fontFamily: "Serif"),
+          headline4:
+              TextStyle(color: kSecondColor, fontSize: 18, fontFamily: "Serif"),
+        ),
       ),
       home: const HomePage(),
     );
